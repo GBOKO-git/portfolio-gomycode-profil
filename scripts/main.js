b92d3b66da7e5eaa8a39481a56db35b3619bda5a
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-02-27 16:54:02 +0000

    cimmit final

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Exemple d'animation ou d'interaction avec le DOM
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector("button");
+  button.addEventListener("click", function () {
+    alert("Merci de me contacter!");
+  });
+});
